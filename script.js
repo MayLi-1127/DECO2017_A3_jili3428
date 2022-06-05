87e6054a514434e4fa0e0759309a24ae3e6c6f4c
MayLi-1127 <lijingjie1998@gmail.com>
2022-06-05 21:53:12 +0800

    Final Change

--- script.js
+++ script.js
@@ -1,23 +1,106 @@
+var sti;
+function startTime(){
+        var clock = document.getElementById("clock");
+        function initNumXY(){
+            var radius = 120;
+            var dot_num = 360/$(".dot").length;
+
+            var ahd = dot_num*Math.PI/180;
+            $(".dot").each(function(index, el) {
+                $(this).css({
+                    "left":180+Math.sin((ahd*index))*radius,
+                    "top":180+Math.cos((ahd*index))*radius
+                });
+            });
+
+            for(var i = 0; i < 12; i++) {
+                clock.innerHTML += "<div class='clock-scale'> " + "<div class='scale-hidden'></div>" +
+                "<div class='scale-show'></div>" + "</div>";
+            }
+            var scale = document.getElementsByClassName("clock-scale");
+                for(var i = 0; i < scale.length; i++) {
+                    scale[i].style.transform="rotate(" + (i * 30) + "deg)";
+            }
+        }
+        initNumXY();
+
+        var HourLine = document.getElementById("HourLine"),
+            MinuteLine = document.getElementById("MinuteLine"),
+            SecondLine = document.getElementById("SecondLine"),
+            Hour = document.getElementById("Hour"),
+            Minute = document.getElementById("Minute"),
+            Second = document.getElementById("Second");
+
+        function setTime(){
+            var nowdate = new Date();
+
+            var year = nowdate.getFullYear(),
+                month = nowdate.getMonth()+1,
+                day = nowdate.getDay(),
+                hours = nowdate.getHours(),
+                minutes = nowdate.getMinutes(),
+                seconds = nowdate.getSeconds(),
+                date = nowdate.getDate();
+
+
+            Hour.innerHTML = hours >=10 ? hours : "0"+hours;
+            Minute.innerHTML = minutes >=10 ? minutes : "0"+minutes;
+            Second.innerHTML = seconds >=10 ? seconds : "0"+seconds;
+            var hour_rotate = (hours*30-90) + (Math.floor(minutes / 12) * 6);
+            HourLine.style.transform = 'rotate(' + hour_rotate + 'deg)';
+            MinuteLine.style.transform = 'rotate(' + (minutes*6 - 90) + 'deg)';
+            SecondLine.style.transform = 'rotate(' + (seconds*6 - 90)+'deg)';
+        }
+        setTime();
+        sti= setInterval(setTime, 1000);
+}
+
+function startClock(){
+    startTime();
+}
+function stopClock(){
+    clearInterval(sti);
+}
+
 function playMusic(){
     $("audio")[0].play();
+    $("#PlaySvg").hide();
+    $("#StopSvg").show();
+}
+
+function stopMusic(){
+    $("audio")[0].pause();
+    $("#PlaySvg").show();
+    $("#StopSvg").hide();
 }
 
 function changeMusic(){
-    var src=$("AlbumCover").attr("src");
+    var src=$("#AlbumCover").attr("src");
+    console.log(src);
     if(src=="Image/BLUE.JPG"){
-        $("AlbumCover").attr("src", "Image/LOVE.JPG");
-        $("MusicName").html("LOVE-May Li");
+        $("#AlbumCover").attr("src", "Image/LOVE.JPG");
+        $("#MusicName").html("LOVE-May Li");
         $("audio").attr("src", "Music/LOVE.mp3");
         $("audio")[0].play();
+        $("#PlaySvg").hide();
+        $("#StopSvg").show();
+
     }else{
-        $("AlbumCover").attr("src", "Image/BLUE.JPG");
-        $("MusicName").html("BLUE-May Li");
+        $("#AlbumCover").attr("src", "Image/BLUE.JPG");
+        $("#MusicName").html("BLUE-May Li");
         $("audio").attr("src", "Music/BLUE.mp3");
         $("audio")[0].play();
+        $("#PlaySvg").hide();
+        $("#StopSvg").show();
     }
 }
 
+$(".ColorList1").click(function(){
+    $(".TaskInfo ul li").show();
+});
+
 $(".ColorList2").click(function(){
+    console.log("aaa");
     $(".Color1").show();
     $(".Color2").hide();
     $(".Color3").hide();
@@ -39,62 +122,11 @@ $(".fa-close").click(function(){
     $(this).parent().parent().hide();
 });
 
-$(function(){
-    var setI;
-    var clock = document.getElementById("clock");
-    function initNumXY(){
-        var radius = 120;
-        var dot_num = 360 / $(".dot").length;
-        var ahd = dot_num * Math.PI / 180;
-        $(".dot").each(function(index, el){
-            $(this).css({
-                "left": 180 + Math.sin((ahd * index)) * radius,
-                "top": 180 + Math.cos((ahd * index)) * radius
-            });
-        });
-        
-        for(var i = 0; i < 12; i++){
-            clock.innerHTML += "<div class='clock-scale'>" + "<div class='scale-hidden'></div>" + "<div class='scale-show'></div>" + "</div>";
-        }
-        var scale = document.getElementsByClassName("clock-scale");
-        for(var i = 0; i < scale.length; i++){
-            scale[i].getElementsByClassName.transform="rotate(" + (i * 30) + "deg)";
-        }
-    }
-    initNumXY();
-    var HourLine = document.getElementById("HourLine"), MinuteLine = document.getElementById("MinuteLine"),
-    SecondLine = document.getElementById("SecondLine"), Hour = document.getElementById("Hour"),
-    Minute = document.getElementById("Minute"), Second = document.getElementById("Second");
-
-    function setTime(){
-        var nowdate = new Date();
-        var hours = nowdate.getHours(), minutes = nowdate.getMinutes(), seconds = nowdate.getDate();
-        Hour.innerHTML = hours >= 10 ? hours : "0" + hours;
-        Minute.innerHTML = minutes >= 10 ? minutes : "0" + minutes;
-        Second.innerHTML = seconds >= 10 ? seconds : "0" + seconds;
-
-        var Hour_rotate = (hours * 30 - 90) + (Math.floor(minutes / 12) * 6);
-        HourLine.style.transform = 'rotate(' + Hour_rotate + 'deg)';
-        MinuteLine.style.transform = 'rotate(' + (minutes * 6 - 90) + 'deg)';
-        SecondLine.style.transform = 'rotate(' + (seconds * 6 - 90) + 'deg)';
-    }
-    setTime();
-    setI = setInterval(setTime, 1000);
-});
-
-
-function startClock(){
-    setI = setInterval(setTime, 1000);
-}
-function stopClock(){
-    clearInterval(setI);
-}
-
 $("#SearchIcon").click(function(){
     var SearchInput = $("#SearchInput").val();
     if(SearchInput==""){
-        $("Tips").html("Please type your words!");
+        $(".Tips").html("Please type your words!");
     }else{
-        $("Tips").html("No result!");
+        $(".Tips").html("No result!");
     }
 });
